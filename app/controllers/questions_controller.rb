62c2b1b72885990ee3f20acf11cfa6a15ccb01bf
Kaif Ahmed <hello@kaif.me>
2018-11-05 13:19:33 +0000

    Implemented rails site

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params["question"] == "I am going to work"
+      @coach_response = "Great!"
+    elsif params["question"].include? "?"
+      @coach_response = "Silly question, get dressed and go to work!"
+    else
+      @coach_response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
